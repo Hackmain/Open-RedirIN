347a85bad6ff2a7df47dcd7d56ec0d733e11dd62
Man Sec <85976822+Hackmain@users.noreply.github.com>
2024-10-06 22:03:15 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install requirements
+pip install -r requirements.txt
+
+# Run the script
+#python open_redirect_scanner.py $1 $2 --output $3 --delay $4 --retries $5 --timeout $6 --verbose $7
